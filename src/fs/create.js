34f420f96cfca212a8bf1db1ba11b2471181e530
Anton Halauko <anton.halauko.main@gmail.com>
2024-01-26 08:57:37 +0200

    refactor: move check file or folder existence function to utils

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs/promises';
-import { getDirname } from './utils.js';
+import { getDirname, checkIfFileOrFolderExist } from './utils.js';
 
 const FILE_NAME_TO_CREATE = 'fresh.txt';
 const FILE_CONTENT = 'I am fresh and young';
@@ -8,22 +8,13 @@ const FILE_CONTENT = 'I am fresh and young';
 const create = async () => {
     const dirname = getDirname()
     const filePath = path.join(dirname, 'files', FILE_NAME_TO_CREATE);
-    const doesExist = await doesFileExist(filePath);
+    const doesExist = await checkIfFileOrFolderExist(filePath);
 
     if (doesExist) {
         throw new Error('FS operation failed');
     }
 
     await fs.writeFile(filePath, FILE_CONTENT)
-};
-
-const doesFileExist = async filePath => {
-    try {
-        const result = await fs.access(filePath, fs.constants.F_OK);
-        return true;
-    } catch (err) {
-        return false;
-    }
 };
 
 await create();
--- src/fs/utils.js
+++ src/fs/utils.js
@@ -1,7 +1,17 @@
 import * as path from 'path';
+import * as  fs from 'fs/promises';
 import { fileURLToPath } from 'node:url';
 
 export const getDirname = () => {
 	const fileName = fileURLToPath(import.meta.url);
-    return path.dirname(fileName);
-}
+	return path.dirname(fileName);
+};
+
+export const checkIfFileOrFolderExist = async pathToCheck => {
+	try {
+		await fs.access(pathToCheck, fs.constants.F_OK);
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
