9ce70f21ffc6ac831637e0bb7b299fbd8b305e49
Anton Halauko <anton.halauko.main@gmail.com>
2024-01-26 16:41:47 +0200

    feat: implement read fs task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs/promises';
+import * as  path from 'path';
+import { FILES_FOLDER_NAME, FS_ERROR_MESSAGE } from './constants.js';
+import { getDirname, checkIfFileOrFolderExist } from './utils.js';
+
+const FILE_TO_READ_NAME = 'fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    const dirname = getDirname();
+    const fileToReadPath = path.join(dirname, FILES_FOLDER_NAME, FILE_TO_READ_NAME);
+    const doesFileExist = await checkIfFileOrFolderExist(fileToReadPath);
+
+    if (!doesFileExist) {
+        throw new Error(FS_ERROR_MESSAGE);
+    }
+
+    const fileContent = await fs.readFile(fileToReadPath);
+    console.log(fileContent.toString());
 };
 
 await read();
