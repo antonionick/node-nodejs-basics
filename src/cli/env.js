95f08e0b0a5c8978c724ee8fd1566039f7856473
Anton Halauko <anton.halauko.main@gmail.com>
2024-01-26 18:19:35 +0200

    feat: implement env cli task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import * as process from 'node:process';
+
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    const valueToLog = process.argv
+        .slice(2)
+        .filter(value => value.startsWith(PREFIX))
+        .reduce((acc, value) => acc ? `${acc}; ${value}` : value, '');
+
+    console.log(valueToLog);
 };
 
 parseEnv();
