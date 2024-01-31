03c55b5bca58fda0173a0eb29c1a8e2d58c8dc28
Anton Halauko <anton.halauko.main@gmail.com>
2024-01-31 09:18:14 +0200

    fix: change env cli task implementation

--- src/cli/env.js
+++ src/cli/env.js
@@ -3,9 +3,9 @@ import * as process from 'node:process';
 const PREFIX = 'RSS_';
 
 const parseEnv = () => {
-    const valueToLog = process.argv
-        .slice(2)
-        .filter(value => value.startsWith(PREFIX))
+    const valueToLog = Object.keys(process.env)
+        .filter(key => key.startsWith(PREFIX))
+        .map(key => `${key}=${process.env[key]}`)
         .join('; ');
 
     console.log(valueToLog);
